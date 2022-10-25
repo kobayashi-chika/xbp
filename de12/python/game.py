be8b9d2a0b87e942cad3ba119bb18ea73cf02429
kobayashi-chika <r202201865tk@jindai.jp>
2022-10-25 16:53:50 +0900

    202210251

--- de12/python/game.py
+++ de12/python/game.py
@@ -1,7 +1,11 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
+from numpy import size
+あなたが飼いたいペットを生成します。
 
-from sklearn import datasets
-from sklearn.neural_network import MLPClassifier
-from sklearn.model_selection import train_test_split
+name=input("名前をつけてください。")
+ookisa=input("大きさは？1-5(小さい⇔大きい)で答えてください。")
+age=input("年齢は？1-5(若い⇔老いている)で答えてください。")
+kawaisa=input("かわいさは？1-5(かわいい⇔かっこいい）で答えてください。")
+seibetu=input("性別は？オス=１、メス=２で答えてください。")
+seikaku=input("性格は？１＝甘えん坊、２＝勇敢、３＝臆病、４＝狂暴、５＝ツンデレ")
+tokutyou=input("特徴？１＝食いしん坊、２＝散歩好き、３＝、４＝、５＝")
+print(ookisa,"の"name, "さんは腹囲", , "cmで年齢は",age, "才ですね。")
--- de12/python/work4.py
+++ de12/python/work4.py
@@ -9,3 +9,4 @@ if waist>=85 and age>=40:
     print(name,"さん、内臓脂肪蓄積注意です")
 else:
     print(name,"さん、腹囲は問題ありません")
+
