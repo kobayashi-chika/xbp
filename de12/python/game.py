468ac53189f16014ca033a33ca261e1854c801cc
kobayashi-chika <r202201865tk@jindai.jp>
2022-10-25 16:53:35 +0900

    20221025

---
+++ de12/python/game.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from sklearn import datasets
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import train_test_split
